e7ae06970e2f2127217cfc1f686c601ed670133e
Nouman Ghauri <muhammadnoumanghauri@gmail.com>
2024-05-06 19:05:40 +0500

    Username sa Email

--- app/src/main/java/com/example/mynutrition/Homepage.java
+++ app/src/main/java/com/example/mynutrition/Homepage.java
@@ -7,18 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.FragmentManager;
-
-import com.example.mynutrition.databinding.ActivityHomepageBinding;
 
 public class Homepage extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-
+        setContentView(R.layout.activity_homepage);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
