e63018e88c45da10d25340206f19aa610360492e
zeptodan <muhammadannas138@gmail.com>
2024-05-15 15:26:37 +0500

    chat update

--- app/src/main/java/com/example/mynutrition/MessageAdapter.java
+++ app/src/main/java/com/example/mynutrition/MessageAdapter.java
@@ -8,22 +8,41 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.ArrayList;
 
 public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder>{
 
     ArrayList<Message> messages = new ArrayList<>();
+    String userid = FirebaseAuth.getInstance().getCurrentUser().getUid();
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.message,parent,false);
+        View view;
+        if (viewType == 0)
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.message,parent,false);
+        else
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.receivermessage,parent,false);
         ViewHolder Holder = new ViewHolder(view);
         return Holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.message.setText(messages.get(position).getText());
+        if (userid.equals(messages.get(position).getSenderId()))
+            holder.message.setText(messages.get(position).getText());
+        else
+            holder.receivermessage.setText(messages.get(position).getText());
+
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser().equals(messages.get(position).getSenderId()))
+            return 0;
+        return 1;
     }
 
     @Override
@@ -37,9 +56,11 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView message;
+        private TextView receivermessage;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             message = itemView.findViewById(R.id.message);
+            receivermessage = itemView.findViewById(R.id.receivermessage);
         }
     }
 }
