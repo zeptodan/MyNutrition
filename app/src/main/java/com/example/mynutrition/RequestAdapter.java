032de4ce15d7fac9b75b49eae8abe352ff968421
zeptodan <muhammadannas138@gmail.com>
2024-05-18 23:47:13 +0500

    stuff

---
+++ app/src/main/java/com/example/mynutrition/RequestAdapter.java
@@ -0,0 +1,82 @@
+package com.example.mynutrition;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mynutrition.ui.appointments.AppointmentFragment;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+
+public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.ViewHolder>{
+
+    ArrayList<Person> people = new ArrayList<>();
+    Context context;
+    String userid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+    public RequestAdapter(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.select,parent,false);
+        ViewHolder Holder = new ViewHolder(view);
+        return Holder;
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.selecttextname.setText(people.get(position).getName());
+        holder.selecttextphone.setText(people.get(position).getPhonenum());
+        holder.select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chat=new Intent(context, Chat.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("id", people.get(holder.getAdapterPosition()).getId());
+                chat.putExtras(bundle);
+                context.startActivity(chat);
+            }
+        });
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return people.size();
+    }
+
+    public void setPeople(ArrayList<Person> people) {
+        this.people = people;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private CardView select;
+        private TextView selecttextname;
+        private TextView selecttextphone;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            select = itemView.findViewById(R.id.selectcard);
+            selecttextname = itemView.findViewById(R.id.selecttextname);
+            selecttextphone = itemView.findViewById(R.id.selecttextphone);
+        }
+    }
+}
+
+
--- app/src/main/java/com/example/mynutrition/ui/appointments/AppointmentFragment.java
+++ app/src/main/java/com/example/mynutrition/ui/appointments/AppointmentFragment.java
@@ -42,4 +42,9 @@ public class AppointmentFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_appointment, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
