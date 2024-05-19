99ffe3cbdde789a5c52381e13cd5802e17c510a9
zeptodan <muhammadannas138@gmail.com>
2024-05-19 12:35:26 +0500

    nutritionist sign up

--- app/src/main/java/com/example/mynutrition/RegisterNutritionist.java
+++ app/src/main/java/com/example/mynutrition/RegisterNutritionist.java
@@ -1,16 +1,24 @@
 package com.example.mynutrition;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterNutritionist extends AppCompatActivity {
     TextInputEditText instituteName_v, CNIC_v;
@@ -42,9 +50,27 @@ public class RegisterNutritionist extends AppCompatActivity {
         if(institureName.isEmpty())
         {
             Toast.makeText(this, "Invalid Institute name", Toast.LENGTH_SHORT).show();
+            return;
         } else if (CNIC.isEmpty()||CNIC.length()<11) {
             Toast.makeText(this, "Invalid CNIC", Toast.LENGTH_SHORT).show();
+            return;
         }
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        db.getReference("user").child(auth.getCurrentUser().getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                User user = task.getResult().getValue(User.class);
+                Nutritionist nutritionist = new Nutritionist(user.getName(),user.getPhonenum(),user.getId(),false,CNIC,institureName);
+                db.getReference("nutritionist").child(user.getId()).setValue(nutritionist).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent home=new Intent(RegisterNutritionist.this, Homepage.class);
+                        startActivity(home);
+                        finish();
+                    }
+                });
+            }
+        });
     }
-
 }
